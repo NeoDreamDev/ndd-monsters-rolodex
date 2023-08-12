ae802ef6c3822ba1c0b0cebe323d96a1c99de6b4
neodreamdev <salaamwilliams@gmail.com>
2023-08-12 07:57:13 -0400

    Adding the full list of monsters into the state. Stored the string into the componenet state. which allowed me to filter from the original list of monsters.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -9,10 +8,13 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: '',
     };
+    console.log('constructor');
   }
 
   componentDidMount() {
+    console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) =>
@@ -28,9 +30,27 @@ class App extends Component {
   }
 
   render() {
+    console.log('render');
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className='App'>
-        {this.state.monsters.map((monster) => {
+        <input
+          className='search-box'
+          type='search'
+          placeholder='Search monsters'
+          onChange={(event) => {
+            const searchField = event.target.value.toLocaleLowerCase();
+            this.setState(() => {
+              return { searchField };
+            });
+          }}
+        />
+
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
